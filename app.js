304350fd5f78e50fb26a8b381a961ef4ad110af7
mreed5812 <mreed5812@gmail.com>
2019-09-08 11:17:05 -0500

    added function to get weather api data

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const searchButton = document.getElementById('searchButton');
 var searchItem;
+//api key 1b23dcb87aa42a7d93f01fef6cf5050b
+
+const displayWeather = document.getElementById('weatherDisplay');
 
 searchButton.addEventListener('click', function () {
     searchItem = document.getElementById('searchInput').value;
-    console.log(searchItem);
+    getWeatherStatus();
 });
+
+
+async function getWeatherStatus() {
+    try {
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1b23dcb87aa42a7d93f01fef6cf5050b', { mode: 'cors' })
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (response) {
+                var weatherData = response
+                console.log(weatherData.weather[0].description)
+                // console.log(response.coord)
+                //console.log(response.coord.weather.description)
+            })
+    } catch (error) {
+        console.log('oops');
+    }
+}
+
