9b382a6af8dd2d1f66c8d132336c7601d9af1d00
mreed5812 <mreed5812@gmail.com>
2019-09-08 16:03:02 -0500

    added function to display weather status

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const searchButton = document.getElementById('searchButton');
-var searchItem;
 //api key 1b23dcb87aa42a7d93f01fef6cf5050b
-
+var city;
+var weatherDescription;
 const displayWeather = document.getElementById('weatherDisplay');
 
+//On click, get city from input
 searchButton.addEventListener('click', function () {
-    searchItem = document.getElementById('searchInput').value;
-    getWeatherStatus();
+    city = document.getElementById('searchInput').value;
+    const result = getWeatherData();
+    result.then(data => displayWeatherData(data));
 });
 
 
-async function getWeatherStatus() {
-    try {
-        fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1b23dcb87aa42a7d93f01fef6cf5050b', { mode: 'cors' })
-            .then(function (response) {
-                return response.json()
-            })
-            .then(function (response) {
-                var weatherData = response
-                console.log(weatherData.weather[0].description)
-                // console.log(response.coord)
-                //console.log(response.coord.weather.description)
-            })
-    } catch (error) {
-        console.log('oops');
-    }
+//get city weather data from api
+async function getWeatherData() {
+    let response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=1b23dcb87aa42a7d93f01fef6cf5050b', { mode: 'cors' })
+    let weatherData = await response.json()
+    console.log(weatherData.weather[0].description);
+    let data = weatherData.weather[0].description;
+    return data;
+
 }
+
+function displayWeatherData(weatherStatus) {
+    displayWeather.innerHTML = weatherStatus;
+}
+
 
