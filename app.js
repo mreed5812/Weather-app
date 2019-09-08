7a0bf8096b1292660a870cb86cb2c0d0e99dd190
mreed5812 <mreed5812@gmail.com>
2019-09-07 22:35:13 -0500

    setup files

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("test");
+const searchButton = document.getElementById('searchButton');
+var searchItem;
+
+searchButton.addEventListener('click', function () {
+    searchItem = document.getElementById('searchInput').value;
+    console.log(searchItem);
+});
