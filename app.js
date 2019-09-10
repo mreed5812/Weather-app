6195ab797ee73a6b8131aa677e015ee67182d7cc
mreed5812 <mreed5812@gmail.com>
2019-09-09 21:37:56 -0500

    refactored city weather search

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
+var apikey = '1b23dcb87aa42a7d93f01fef6cf5050b';
 const searchButton = document.getElementById('searchButton');
-//api key 1b23dcb87aa42a7d93f01fef6cf5050b
-var city;
-var weatherDescription;
 const displayWeather = document.getElementById('weatherDisplay');
 
 //On click, get city from input
 searchButton.addEventListener('click', function () {
-    city = document.getElementById('searchInput').value;
-    const result = getWeatherData();
+    let city = document.getElementById('searchInput').value;
+    const result = getWeatherData(city);
     result.then(data => displayWeatherData(data));
 });
 
-
 //get city weather data from api
-async function getWeatherData() {
-    let response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=1b23dcb87aa42a7d93f01fef6cf5050b', { mode: 'cors' })
+async function getWeatherData(city) {
+    let response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=' + apikey, { mode: 'cors' })
     let weatherData = await response.json()
-    console.log(weatherData.weather[0].description);
+    //console.log(weatherData.weather[0].description);
     let data = weatherData.weather[0].description;
     return data;
-
 }
 
 function displayWeatherData(weatherStatus) {
