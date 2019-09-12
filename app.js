266a0ce996be42a1e888190c78528b516e0445e2
mreed5812 <mreed5812@gmail.com>
2019-09-11 20:49:54 -0500

    added forecast city location into description

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ var apikey = '1b23dcb87aa42a7d93f01fef6cf5050b';
 const searchButton = document.getElementById('searchButton');
 const displayWeather = document.getElementById('weatherDisplay');
 
+
 //On click, get city from input
 searchButton.addEventListener('click', function () {
-    let city = document.getElementById('searchInput').value;
+    let city = getInputCity();
+    clearInput();
     const result = getWeatherData(city);
-    result.then(data => displayWeatherData(data));
+    result.then(data => displayWeatherData(data, city));
     result.then(data => getWeatherGif(data));
 });
+
+function getInputCity() {
+    let city = document.getElementById('searchInput').value;
+    return city;
+}
+
+function clearInput() {
+    document.getElementById('searchInput').value = '';
+}
 
 //get city weather data from api
 async function getWeatherData(city) {
@@ -30,8 +41,8 @@ async function getWeatherGif(weatherDescription) {
     img.src = gifData.data.images.original.url;
 }
 
-function displayWeatherData(weatherStatus) {
-    displayWeather.innerHTML = weatherStatus;
+function displayWeatherData(weatherStatus, city) {
+    displayWeather.innerHTML = "Today's forecast for " + city + " : " + weatherStatus;
 }
 
 
